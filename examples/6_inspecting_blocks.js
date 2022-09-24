3b508a2feba41d5d961c5c46575c36150becba47
Ivan Lieskov <ivan.lieskov@protonmail.com>
2022-09-24 19:33:55 +0200

    inspecting blocks

---
+++ examples/6_inspecting_blocks.js
@@ -0,0 +1,23 @@
+const { ethers } = require("ethers");
+
+const INFURA_ID = "7a21468ba350491ab646dc285a57277d";
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://goerli.infura.io/v3/${INFURA_ID}`
+);
+
+const main = async () => {
+  const block = await provider.getBlockNumber();
+
+  console.log(`\nBlock Number: ${block}\n`);
+
+  const blockInfo = await provider.getBlock(block);
+
+  console.log(blockInfo);
+
+  const { transactions } = await provider.getBlockWithTransactions(block);
+
+  console.log(`\nLogging first transaction in block:\n`);
+  console.log(transactions[0]);
+};
+
+main();
